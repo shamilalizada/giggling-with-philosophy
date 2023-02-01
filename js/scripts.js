0be37a9f6243eb05e46efef7f0e17a5e15c384a4
shamilalizada <121141227+shamilalizada@users.noreply.github.com>
2023-01-31 18:48:36 -0800

    three pokemon  created,

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-let pokemonList= [ ]
+let pokemonList= a; 
+pokemons=[ {name: "Bulbasaur", height: "7", type: "grass"}, {name: "eve", height: "7", type:["water","speed"]}, {name: "Charmander", height: "7", types: "fire"} ]
