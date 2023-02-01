8eb0d22b4e9816dd3f7e4ef92f82eadf204d7acd
shamilalizada <121141227+shamilalizada@users.noreply.github.com>
2023-01-31 19:00:19 -0800

    adjustment has been made, each objects  stacked on as opposed to having a long list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-let pokemonList=[ {name: "Bulbasaur", height: "7", type: "grass"}, {name: "eve", height: "7", type:["water","speed"]}, {name: "Charmander", height: "7", types: "fire"} ]
+let pokemonList=[ 
+    {name: "Bulbasaur", height: "7", type: ["grass"]},
+    {name: "eve", height: "7", type:["water","speed"]}, 
+    {name: "Charmander", height: "7", types: ["fire"]},
+ ];
+
 console.log(pokemonList);
 pokemonList[0];
 pokemonList[1];
