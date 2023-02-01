6050619a8f210f1d522a2b655c3fa9b4ba893770
shamilalizada <121141227+shamilalizada@users.noreply.github.com>
2023-01-31 18:53:21 -0800

    variables created

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-let pokemonList= a; 
-pokemons=[ {name: "Bulbasaur", height: "7", type: "grass"}, {name: "eve", height: "7", type:["water","speed"]}, {name: "Charmander", height: "7", types: "fire"} ]
+let pokemonList=[ {name: "Bulbasaur", height: "7", type: "grass"}, {name: "eve", height: "7", type:["water","speed"]}, {name: "Charmander", height: "7", types: "fire"} ]
+console.log(pokemonList);
+pokemonList[0];
+pokemonList[1];
+pokemonList[2];
+
